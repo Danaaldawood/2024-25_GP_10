9ae5453ff87b033d43ab7b49b6ebe38aa39983cf
Haya-Alhawaimel <443200917@student.ksu.edu.sa>
2024-10-13 20:22:52 +0300

    ..

--- src/Fpage/Fpage.jsx
+++ src/Fpage/Fpage.jsx
@@ -1,47 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
 import LOGO from '../images/Logo.png';
 import photo from '../images/MAP-logo.png';
 import './Fpage.css';
 import 'chart.js/auto';
+ import '../Header.css';   //Header style//
 
-const HomePage = () => {
+ 
+
+const Fpage = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+ 
+
   return (
-    <div className="homepage">
+    <div>
       {/* Header */}
       <header className="header">
-        <div className="header-left ">
-          <img src={LOGO} alt="CultureLens Logo" className="logo-image" />
-          <h1 className="site-title">CultureLens</h1>
+        <div className="header-left">
+          <img src={LOGO} alt="CultureLens Logo" className="logo-img" />
+          <h1 className="logo-title">CultureLens</h1>
         </div>
 
-      
+         
       </header>
+
+      
 
       {/* About us section */}
       <div className="info-section">
         <div className="text-content">
-          <p className="section-title"  >Welcome to CultureLens!</p>
+          <p className="section-title">Welcome to CultureLens!</p>
           <p className="section-description">
             Begin your journey in understanding global cultures and discover how language models interact with diverse values and standards.
-            Ready to explore cultures in new ways? <p>Get started now!</p>
+            Ready to explore cultures in new ways? <br />Get started now!
           </p>
           <nav className="nav-buttons">
-          <Link to="/Sign">
-            <button>Get Started</button>
-          </Link>
-          <Link to="/Login">
-            <button>Log in</button>
-          </Link>
-        </nav>
+            <Link to="/Sign">
+              <button className='BFpage'>Get Started</button>
+            </Link>
+            <Link to="/Login">
+              <button className='BFpage'>Log in</button>
+            </Link>
+          </nav>
         </div>
         <img src={photo} alt="Map Logo" className="animated-logo" />
       </div>
-
-        
     </div>
   );
 };
 
-export default HomePage;
+export default Fpage;
