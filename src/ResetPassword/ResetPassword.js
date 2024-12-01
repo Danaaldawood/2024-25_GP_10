feb09aa32963592d783e51bddb7d3f743bc79acd
REEMAK21 <alkhaldyryma657@gmail.com>
2024-12-01 05:07:20 +0300

    :)

--- src/ResetPassword/ResetPassword.js
+++ src/ResetPassword/ResetPassword.js
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
-import { getAuth, confirmPasswordReset } from 'firebase/auth';
-import { useLocation, useNavigate } from 'react-router-dom';
-import './ResetPassword.css';
-import LOGO from '../images/Logo.png';
-import '../Register/Pop-Message.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash ,faCheckCircle} from '@fortawesome/free-solid-svg-icons';
-import { Helmet } from 'react-helmet';
-
+import React, { useState } from "react";
+import { getAuth, confirmPasswordReset } from "firebase/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import "./ResetPassword.css";
+import LOGO from "../images/Logo.png";
+import "../Register/Pop-Message.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEye,
+  faEyeSlash,
+  faCheckCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { Helmet } from "react-helmet";
 
 export const ResetPassword = () => {
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
   const [showSuccess, setShowSuccess] = useState(false);
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  // Retrieve query parameters from URL
+
   const location = useLocation();
   const navigate = useNavigate();
   const query = new URLSearchParams(location.search);
-  const oobCode = query.get('oobCode');
+  const oobCode = query.get("oobCode");
+  // Password validation
 
   const isMinCharacters = newPassword.length >= 8;
   const hasUppercase = /[A-Z]/.test(newPassword);
   const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(newPassword);
   const isPasswordValid = isMinCharacters && hasUppercase && hasSpecialChar;
   const doPasswordsMatch = newPassword === confirmPassword;
+  // Toggle visibility of password
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -35,37 +42,45 @@ export const ResetPassword = () => {
   const toggleConfirmPasswordVisibility = () => {
     setShowConfirmPassword(!showConfirmPassword);
   };
-
+  // Handle form submission for resetting passwor
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');
+    setError("");
     setLoading(true);
 
     if (!isPasswordValid) {
       let errorMessages = [];
-      if (!isMinCharacters) errorMessages.push("Password must be at least 8 characters.");
-      if (!hasUppercase) errorMessages.push("Password must contain at least one uppercase letter.");
-      if (!hasSpecialChar) errorMessages.push("Password must contain at least one special character.");
+      if (!isMinCharacters)
+        errorMessages.push("Password must be at least 8 characters.");
+      if (!hasUppercase)
+        errorMessages.push(
+          "Password must contain at least one uppercase letter."
+        );
+      if (!hasSpecialChar)
+        errorMessages.push(
+          "Password must contain at least one special character."
+        );
       setError(errorMessages.join(" "));
       setLoading(false);
       return;
     }
 
     if (!doPasswordsMatch) {
-      setError('Passwords do not match.');
+      setError("Passwords do not match.");
       setLoading(false);
       return;
     }
+    // Perform password reset using Firebase API
 
     const auth = getAuth();
     try {
       await confirmPasswordReset(auth, oobCode, newPassword);
       setShowSuccess(true);
       setTimeout(() => {
-        navigate('/');
+        navigate("/");
       }, 3000);
     } catch (err) {
-      setError('Error updating password. Please try again.');
+      setError("Error updating password. Please try again.");
     } finally {
       setLoading(false);
     }
@@ -74,42 +89,53 @@ export const ResetPassword = () => {
   return (
     <div className="reset-password-page">
       <Helmet>
-      <title>Rest Password</title>
-      <meta name="description" content="Rest Password page" />
-    </Helmet>
+        <title>Rest Password</title>
+        <meta name="description" content="Rest Password page" />
+      </Helmet>
+      {/* Display error messages */}
+
       {error && (
         <div className="error-popup">
           <h3 className="error-title">Warning!</h3>
           <p className="error-message">{error}</p>
           <div className="error-actions">
-            <button className="confirm-btn" onClick={() => setError('')}>Try again</button>
+            <button className="confirm-btn" onClick={() => setError("")}>
+              Try again
+            </button>
           </div>
         </div>
       )}
-      
+      {/* Display success message */}
+
       {showSuccess && (
         <div className="success-popup">
           <FontAwesomeIcon icon={faCheckCircle} className="success-icon" />
           <p className="success-message">Password updated successfully!</p>
-          <div className="success-actions">
-            
-          </div>
+          <div className="success-actions"></div>
         </div>
       )}
+      {/* Password reset form */}
 
       <div className="reset-password-container">
         <div className="reset-password-left-section">
+          {/* Logo and introductory text */}
+
           <div className="reset-password-logo-welcome-container">
             <img src={LOGO} alt="Logo" width="100" height="100" />
             <h2>Reset Password</h2>
           </div>
-          <p className="reset-password-welcome-txt">Enter your new password to regain access to your account.</p>
+          <p className="reset-password-welcome-txt">
+            Enter your new password to regain access to your account.
+          </p>
         </div>
 
         <form className="reset-password-form" onSubmit={handleSubmit}>
           <h2 className="reset-password-title">Change Password</h2>
-          
-          <label className="reset-password-label" htmlFor="newPassword">New Password:</label>
+          {/* New password input */}
+
+          <label className="reset-password-label" htmlFor="newPassword">
+            New Password:
+          </label>
           <div className="reset-password-input-container">
             <input
               type={showPassword ? "text" : "password"}
@@ -120,24 +146,31 @@ export const ResetPassword = () => {
               placeholder="Enter new password"
               required
             />
-            <span onClick={togglePasswordVisibility} className="reset-password-icon">
+            <span
+              onClick={togglePasswordVisibility}
+              className="reset-password-icon"
+            >
               <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
             </span>
           </div>
+          {/* Password requirements */}
 
           <ul className="password-requirements">
-            <li className={isMinCharacters ? 'valid' : 'invalid'}>
+            <li className={isMinCharacters ? "valid" : "invalid"}>
               ✔ Password should be at least 8 characters.
             </li>
-            <li className={hasUppercase ? 'valid' : 'invalid'}>
+            <li className={hasUppercase ? "valid" : "invalid"}>
               ✔ Contain at least one uppercase letter.
             </li>
-            <li className={hasSpecialChar ? 'valid' : 'invalid'}>
+            <li className={hasSpecialChar ? "valid" : "invalid"}>
               ✔ Contain at least one special character.
             </li>
           </ul>
-          
-          <label className="reset-password-label" htmlFor="confirmPassword">Confirm Password:</label>
+          {/* Confirm password input */}
+
+          <label className="reset-password-label" htmlFor="confirmPassword">
+            Confirm Password:
+          </label>
           <div className="reset-password-input-container">
             <input
               type={showConfirmPassword ? "text" : "password"}
@@ -148,22 +181,28 @@ export const ResetPassword = () => {
               placeholder="Confirm new password"
               required
             />
-            <span onClick={toggleConfirmPasswordVisibility} className="reset-password-icon">
-              <FontAwesomeIcon icon={showConfirmPassword ? faEye : faEyeSlash} />
+            <span
+              onClick={toggleConfirmPasswordVisibility}
+              className="reset-password-icon"
+            >
+              <FontAwesomeIcon
+                icon={showConfirmPassword ? faEye : faEyeSlash}
+              />
             </span>
           </div>
-          
+          {/* Match validation */}
+
           {confirmPassword && (
             <ul className="password-requirements">
-              <li className={doPasswordsMatch ? 'valid' : 'invalid'}>
+              <li className={doPasswordsMatch ? "valid" : "invalid"}>
                 ✔ Passwords match
               </li>
             </ul>
           )}
 
-          <button 
-            type="submit" 
-            className="reset-password-btn" 
+          <button
+            type="submit"
+            className="reset-password-btn"
             disabled={loading || !isPasswordValid || !doPasswordsMatch}
           >
             {loading ? "Updating..." : "Update Password"}
