56ea3cf2ca540dc8f0d9dbf327a8c7987aec8493
Dana Aldawood <137423752+Danaaldawood@users.noreply.github.com>
2025-05-08 18:43:13 +0300

    ..

--- src/Register/firebase.js
+++ src/Register/firebase.js
@@ -1,4 +1,4 @@
-/* ====== IMPORTS ====== */
+/* ====== IMPORTS ======= */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
