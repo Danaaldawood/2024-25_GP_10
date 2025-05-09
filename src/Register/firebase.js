e84fe2b2065240991a8df2961ae143c8781090fb
Dana Aldawood <137423752+Danaaldawood@users.noreply.github.com>
2025-05-10 02:27:37 +0300

    ...

--- src/Register/firebase.js
+++ src/Register/firebase.js
@@ -1,20 +1,17 @@
-/* ====== IMPORTS ====== */
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getDatabase } from "firebase/database";  // Add this for Realtime Database
+import { getDatabase } from "firebase/database";
 
-/* ====== CONFIG ====== */
 const firebaseConfig = {
-    apiKey: "AIzaSyBtrgrVsEcyhmIcc5eZWW7qYZSSfpwnXOE",
-    authDomain: "login-auth-4736e.firebaseapp.com",
-    projectId: "culturelens-4872c",
-    storageBucket: "login-auth-4736e.appspot.com",
-    messagingSenderId: "10562914305",
-    appId: "1:10562914305:web:2cff37be4fa9ccf0a29800"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-/* ====== INITIALIZATION & EXPORTS ====== */
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
